ccbbaf34188071db4afc2ee64ba994043509f67d
DaveonHMD <129466844+daveonHMD@users.noreply.github.com>
2023-04-20 23:30:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const htmlContent = '<html><body><h1>Hello, World!</h1></body></html>';
+
+let count = 0
+
+const intervalId = setInterval(() => {
+  if (count >= 1) {
+    clearInterval(intervalId);
+    return;
+  }
+
+  fetch('https:/thrivehub.destools.ink/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ htmlContent: htmlContent })
+  })
+    .then(response => response.text())
+    .then(url => {
+      console.log(`Generated URL: ${url}`);
+    })
+    .catch(error => console.log(error));
+
+  count++;
+}, 1000);
